179f602c1426c3413ef20920a92d5a58aa51f40d
hiromzn <hiromzn@gmail.com>
2020-11-18 11:36:27 +0900

    add client argument : ip_address and port

--- example/socket/client.c
+++ example/socket/client.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
  
-int main() {
+int main( int argc, char **argv )
+{
     int sockfd;
     struct sockaddr_in addr;
 	int i;
+	int ret;
+	char *adr_str = "127.0.0.1";
+	int port;
  
+	if (argc < 2) {
+		fprintf( stderr, "usage: %s ip_address port_number\n", *argv );
+		return(1);
+	}
+	adr_str = argv[1];
+	port = atoi( argv[2] );
     // ソケット生成
     if( (sockfd = socket( AF_INET, SOCK_STREAM, 0) ) < 0 ) {
         perror( "socket" );
@@ -17,11 +29,16 @@ int main() {
  
     // 送信先アドレス・ポート番号設定
     addr.sin_family = AF_INET;
-    addr.sin_port = htons( 1234 );
-    addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+    addr.sin_port = htons( port );
+    addr.sin_addr.s_addr = inet_addr( adr_str );
  
     // サーバ接続
-    connect( sockfd, (struct sockaddr *)&addr, sizeof( struct sockaddr_in ) );
+	printf( "connect( %s, %d )\n", adr_str, port );
+    ret = connect( sockfd, (struct sockaddr *)&addr, sizeof( struct sockaddr_in ) );
+	if ( ret < 0 ) {
+		fprintf( stderr, "ERROR: connect() errno:%d\n", errno );
+		return(1);
+	}
  
     // データ送信
     char send_str[10];
