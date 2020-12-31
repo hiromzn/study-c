1cf9ecfa8c7aa5a418a10690fa918dee5cd3642e
hiromzn <hiromzn@gmail.com>
2020-12-31 15:48:01 +0900

    update connect_timeout

--- example/socket/connect_timeout/connect_timeout.c
+++ example/socket/connect_timeout/connect_timeout.c
@@ -57,18 +57,18 @@ char *connect_nonblock_get_error_message( int errid )
 /*
  * NON blocking connect()
  *
- *	 arguments: ( first 3 arguments is equal to connect() arguments )
+ *   arguments: ( first 3 arguments is equal to connect() arguments )
  *
- *	   [IN]	 socket : socket descriptor
- *	   [IN]	 address : address to connect
- *	   [IN]	 address_len : length of address data
+ *     [IN]  socket : socket descriptor
+ *     [IN]  address : address to connect
+ *     [IN]  address_len : length of address data
  *
- *	   [IN]	 timeout : time out value
- *	   [OUT] error_id : error code of this function
+ *     [IN]  timeout : time out value
+ *     [OUT] error_id : error code of this function
  *
- *	 return value:
- *	   0 : success
- *	   -1 : error ( reason of error : check errno and error_id of argument )
+ *   return value:
+ *     0 : success
+ *     -1 : error ( reason of error : check errno and error_id of argument )
  */
 int connect_nonblock( int socket, struct sockaddr *address, int address_len, struct timeval timeout, int *error_id )
 {
@@ -77,108 +77,108 @@ int connect_nonblock( int socket, struct sockaddr *address, int address_len, str
 
 	int		flags = 0;
 	int		result = 0;
-	fd_set	readFd, writeFd, errFd;
+    fd_set	readFd, writeFd, errFd;
 
 	*error_id = 0;
 
-	/* set NON-blocking mode */
-	flags = fcntl(socket, F_GETFL);
-	if ( _FCNTL_ERROR_ == flags )
-	{
+    /* set NON-blocking mode */
+    flags = fcntl(socket, F_GETFL);
+    if ( _FCNTL_ERROR_ == flags )
+    {
 		*error_id = 0x001;
 		_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-		return _ERROR_;
-	}
-	result = fcntl(socket, F_SETFL, flags | O_NONBLOCK);
-	if ( _FCNTL_ERROR_ == result )
-	{
+        return _ERROR_;
+    }
+    result = fcntl(socket, F_SETFL, flags | O_NONBLOCK);
+    if ( _FCNTL_ERROR_ == result )
+    {
 		*error_id = 0x002;
 		_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-		return _ERROR_;
-	}
+        return _ERROR_;
+    }
 
-	/* CONNECT */
-	result = connect(socket, address, address_len);
-	if ( result )
-	{
-		if ( EINPROGRESS == errno )
-		{
-			/*
+    /* CONNECT */
+    result = connect(socket, address, address_len);
+    if ( result )
+    {
+        if ( EINPROGRESS == errno )
+        {
+            /*
 				The socket is non-blocking and the connection cannot be completed immediately.
 				It is possible to select(2) for completion by selecting the socket for writing.
 			*/
-			errno = 0;
-		}
-		else
-		{
+            errno = 0;
+        }
+        else
+        {
 			*error_id = 0x003;
 			/* Actual connect ERROR */
-			/* Fail connect : return to blocking mode */
-			fcntl(socket, F_SETFL, flags );
+            /* Fail connect : return to blocking mode */
+            fcntl(socket, F_SETFL, flags );
 			_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-			return _ERROR_;
-		}
-	}
+            return _ERROR_;
+        }
+    }
 
-	/* return to blocking mode */
-	result = fcntl(socket, F_SETFL, flags );
-	if ( _FCNTL_ERROR_ == result  )
-	{
+    /* return to blocking mode */
+    result = fcntl(socket, F_SETFL, flags );
+    if ( _FCNTL_ERROR_ == result  )
+    {
 		*error_id = 0x004;
 		_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-		return _ERROR_;
-	}
+        return _ERROR_;
+    }
 
-	/* wait by select */
-	FD_ZERO(&readFd);
-	FD_ZERO(&writeFd);
-	FD_ZERO(&errFd);
-	FD_SET(socket, &readFd);
-	FD_SET(socket, &writeFd);
-	FD_SET(socket, &errFd);
-	int sockNum = select( socket + 1, &readFd, &writeFd, &errFd, &timeout );
-	if ( 0 == sockNum )
-	{
-		/* time out */
-		/*	no desctiptor that are contained anything */
+    /* wait by select */
+    FD_ZERO(&readFd);
+    FD_ZERO(&writeFd);
+    FD_ZERO(&errFd);
+    FD_SET(socket, &readFd);
+    FD_SET(socket, &writeFd);
+    FD_SET(socket, &errFd);
+    int sockNum = select( socket + 1, &readFd, &writeFd, &errFd, &timeout );
+    if ( 0 == sockNum )
+    {
+        /* time out */
+		/*  no desctiptor that are contained anything */
 		*error_id = 0x005;
 		_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-		return _ERROR_;
-	}
-	else if ( FD_ISSET(socket, &readFd) || FD_ISSET(socket, &writeFd)  )
-	{
-		/*
+        return _ERROR_;
+    }
+    else if ( FD_ISSET(socket, &readFd) || FD_ISSET(socket, &writeFd)  )
+    {
+        /*
 		  SUCCESS
-			socket is available for read and write
+		    socket is available for read and write
 		*/
-	}
-	else
-	{
+    }
+    else
+    {
 		*error_id = 0x006;
 		_DEBUG_PRINTF( "ERROR: %s : %d\n", GET_ERR_MSG(*error_id), errno );
-		return _ERROR_;
-	}
+        return _ERROR_;
+    }
 
 	/*
 	  check socket error
-		check any pending error on the socket and clears the error status.
+	    check any pending error on the socket and clears the error status.
 	*/
-	int optval = 0;
-	socklen_t optlen = (socklen_t)sizeof(optval);
-	errno = 0;
-	result = getsockopt(socket, SOL_SOCKET, SO_ERROR, (void *)&optval, &optlen);
-	if ( result < 0 )
-	{
-		/* ERROR case */
+    int optval = 0;
+    socklen_t optlen = (socklen_t)sizeof(optval);
+    errno = 0;
+    result = getsockopt(socket, SOL_SOCKET, SO_ERROR, (void *)&optval, &optlen);
+    if ( result < 0 )
+    {
+        /* ERROR case */
 		// _DEBUG_PRINTF( "ERROR: getsockopt 1 : %d\n", errno );
-	}
-	else if ( 0 != optval )
-	{
-		/* ERROR case */
+    }
+    else if ( 0 != optval )
+    {
+        /* ERROR case */
 		// _DEBUG_PRINTF( "ERROR: getsockopt 2 : %d\n", errno );
-	}
+    }
 
-	return 0;
+    return 0;
 }
 
 #ifdef TEST_MAKE
--- example/socket/connect_timeout/main.c
+++ example/socket/connect_timeout/main.c
@@ -6,60 +6,120 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
 extern int connect_nonblock( int , struct sockaddr *, int, struct timeval, int * );
-extern char* connect_nonblock_get_error_message( int );
+extern char *connect_nonblock_get_error_message( int );
  
+#define TCPL_E_ERROR	(-1)
+#define TCPL_S_SUCCESS	(0)
+#define EHD_LOG_P2(a,b,c)	fprintf(stderr,"ERROR:%s:errno=%d\n",b,c);
+
+static void pr_now_time(){
+	struct tm *tmp;
+	struct timeval tv;
+ 
+	gettimeofday(&tv,NULL);
+	tmp=localtime(&tv.tv_sec);
+	printf("%04d/%02d/%02d %02d:%02d:%02d:%3d\n",
+		tmp->tm_year + 1900, tmp->tm_mon + 1,
+		tmp->tm_mday, tmp->tm_hour,
+		tmp->tm_min, tmp->tm_sec,
+		tv.tv_usec/1000);
+} 
+
+int tcpl_sck_settimeout(s, timeout)
+int     s;
+int     timeout;
+{
+#ifndef TCP_USER_TIMEOUT
+	#define TCP_USER_TIMEOUT 18  // how long for loss retry before timeout [ms]
+#endif
+	int     status;
+	int     stimeout = timeout;  // user timeout in milliseconds [ms]
+	status = setsockopt (s, IPPROTO_TCP, TCP_USER_TIMEOUT, (char*) &stimeout, sizeof (stimeout));
+	if (status != 0)        {
+		EHD_LOG_P2 (TCPL_F_FATAL_OS ,"setsockopt (TCP_USER_TIMEOUT)", errno);
+		return (TCPL_E_ERROR);
+	}
+
+	return (TCPL_S_SUCCESS);
+}
+
+#define CHECK_ERROR(ret,str)	if((ret)<0){printf("ERROR : %s :errno=%d\n",(str),errno);perror("ERROR(errno)");}
+
 int main( int argc, char **argv )
 {
-	int sockfd;
-	struct sockaddr_in addr;
+    int sockfd;
+    struct sockaddr_in addr;
 	int i;
 	int ret;
 	char *adr_str = "127.0.0.1";
 	int port;
+	int is_block = 1;
  
-	if (argc < 2) {
-		fprintf( stderr, "usage: %s ip_address port_number\n", *argv );
+	if (argc <= 3) {
+		fprintf( stderr, "usage: %s ip_address port_number is_block\n", *argv );
+		fprintf( stderr, "  is_block : 1..block mode / 0..non block mode\n" );
 		return(1);
 	}
 	adr_str = argv[1];
 	port = atoi( argv[2] );
-	if( (sockfd = socket( AF_INET, SOCK_STREAM, 0) ) < 0 ) {
-		perror( "socket" );
-	}
+	is_block = atoi( argv[3] );
+    // ?????å?����
+    if( (sockfd = socket( AF_INET, SOCK_STREAM, 0) ) < 0 ) {
+        perror( "socket" );
+    }
  
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons( port );
-	addr.sin_addr.s_addr = inet_addr( adr_str );
+    // ?????襢?ɥ쥹???ݡ????ֹ?????
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons( port );
+    addr.sin_addr.s_addr = inet_addr( adr_str );
  
+#define TIME_OUT_SEC	5
+    // ????????³
+	if ( is_block ) {
+		ret = tcpl_sck_settimeout(sockfd, TIME_OUT_SEC * 1000);
+		CHECK_ERROR(ret,"tcpl_sck_settimeout()");
+		printf( "connect( %s, %d ) timeout=%d sec\n", adr_str, port, TIME_OUT_SEC );
+		pr_now_time();
+		ret = connect( sockfd, (struct sockaddr *)&addr, sizeof( struct sockaddr_in ) );
+		pr_now_time();
+		CHECK_ERROR(ret,"connect(BLOCKING)");
+	} else {
 	struct timeval timeout;
 	int error_id = 0;
-	timeout.tv_sec = 5; /* 5 sec */
+	timeout.tv_sec = TIME_OUT_SEC; /* 5 sec */
 	timeout.tv_usec = 0; /* Microseconds. */
-	printf( "connect_nonblock( %s, %d )\n", adr_str, port );
-	ret = connect_nonblock( sockfd, (struct sockaddr *)&addr, sizeof( struct sockaddr_in ), timeout, &error_id );
+	printf( "connect_nonblock( %s, %d ) timeout=%d sec\n", adr_str, port, TIME_OUT_SEC );
+	pr_now_time();
+    ret = connect_nonblock( sockfd, (struct sockaddr *)&addr, sizeof( struct sockaddr_in ), timeout, &error_id );
+	pr_now_time();
 	if ( ret < 0 ) {
 		fprintf( stderr, "ERROR: connect_noblock() : %s : errno=%d\n", connect_nonblock_get_error_message(error_id), errno );
 		if( errno ) perror("connect_nonblock");
-		return(1);
+	}
 	}
  
-	char send_str[10];
-	char receive_str[10];
-	for ( i = 0; i < 10; i++ ){
-		sprintf( send_str, "%d", i );
-		printf( "send:%d\n", i );
-		if( send( sockfd, send_str, 10, 0 ) < 0 ) {
-			perror( "send" );
-		} else {
-			recv( sockfd, receive_str, 10, 0 );
-			printf( "receive:%s\n", receive_str );
-		}
-		sleep( 1 );
-	}
+/*
+    // ?ǡ???????
+    char send_str[10];
+    char receive_str[10];
+    for ( i = 0; i < 10; i++ ){
+        sprintf( send_str, "%d", i );
+        printf( "send:%d\n", i );
+        if( send( sockfd, send_str, 10, 0 ) < 0 ) {
+            perror( "send" );
+        } else {
+            recv( sockfd, receive_str, 10, 0 );
+            printf( "receive:%s\n", receive_str );
+        }
+        sleep( 1 );
+    }
+*/ 
+
+    close( sockfd );
  
-	close( sockfd );
- 
-	return 0;
+    return 0;
 }
