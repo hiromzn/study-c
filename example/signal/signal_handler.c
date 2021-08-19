8021ce8b302985d70be5602f790de96f8b529277
hiromzn <hiromzn@gmail.com>
2021-08-19 17:30:17 +0900

    add signal_handler.c

--- example/signal/signal_handler.c
+++ example/signal/signal_handler.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <signal.h>
 
+void handler(int sig)
+{
+        char buffer[1000];
+        sprintf( buffer, "pstack %d >>/tmp/stack_trace \n", getpid() );
+        system( buffer );
+        printf("hadler last\n");
+        exit(1);
+}
+
+int main()
+{
+        signal(SIGTERM, handler);
+        kill(0, SIGTERM);
+        printf("main last\n");
+}
